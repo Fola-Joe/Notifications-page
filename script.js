42ca7222e314793bd8bfdee3358a60a39ff89238
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-03-15 16:57:51 +0100

    each notification - javascript
    Have been able to work on clicking each notification but have not been able to adjust notification number in accordance

--- script.js
+++ script.js
@@ -3,15 +3,36 @@ const notification = document.querySelector('#notification');
 const notice = document.querySelectorAll('.red-notice');
 const markAll = document.querySelector('h3');
 
-
-
+const unreadMsg = document.querySelectorAll('.unread');
+// notification.innerText = section.length;
 
 markAll.addEventListener('click', function(){
-    section.forEach(function (e) {
-        e.style.backgroundColor = "transparent";
+    unreadMsg.forEach(function (Msg) {
+        Msg.classList.remove('unread');
     })
     notice.forEach(function (e) {
-        e.style.backgroundColor = "transparent";
+        e.classList.remove('red-notice');
     })
-    notification.innerText = '0';
+    const newUnreadMsg = document.querySelectorAll('.unread');
+    notification.innerText = newUnreadMsg.length;
+})
+// markAll.addEventListener('click', function(){
+//     section.forEach(function (e) {
+//         e.classList.remove('unread');
+//     })
+//     notice.forEach(function (e) {
+//         e.classList.remove('red-notice');
+//     })
+//     notification.innerText = '0';
+// })
+
+unreadMsg.forEach(Msg => {
+    Msg.addEventListener('click', () => {
+        Msg.classList.remove('unread');
+        notice.forEach(function (e) {
+            e.classList.remove('red-notice');
+        })
+    })
+    const newUnreadMsg = document.querySelectorAll('.unread');
+    newUnreadMsg.length = notification.innerText ;
 })

549bcb53cc63b454e1f2d953bf530e22371f4843
Fola-Joe <104533211+Fola-Joe@users.noreply.github.com>
2023-03-15 15:32:05 +0100

    (Mark all as read) is done - JS

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const section = document.querySelectorAll('#section');
 const notification = document.querySelector('#notification');
+const notice = document.querySelectorAll('.red-notice');
+const markAll = document.querySelector('h3');
 
 
-section.addEventListener('click', function(){
-    section.style.backgroundColor = 'transparent';
+
+
+markAll.addEventListener('click', function(){
+    section.forEach(function (e) {
+        e.style.backgroundColor = "transparent";
+    })
+    notice.forEach(function (e) {
+        e.style.backgroundColor = "transparent";
+    })
+    notification.innerText = '0';
 })
